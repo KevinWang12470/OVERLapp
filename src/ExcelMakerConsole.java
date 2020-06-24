cb274125c800895445275c9d129232aa03ce3731
Kevin Wang <kevin.wang.1243@gmail.com>
2020-06-24 14:06:11 -0400

    Added working text prompts for schedule info input

--- src/ExcelMakerConsole.java
+++ src/ExcelMakerConsole.java
@@ -18,6 +18,17 @@ public final class ExcelMakerConsole {
     }
 
     /**
+     * Array of the name of class types.
+     */
+    private static final String[] CLASSTYPENAMES = { "Lecture", "Recitation",
+            "Lab" };
+
+    /**
+     * Constant that holds the number of class types.
+     */
+    private static final int NUMOFCLASSTYPES = 3;
+
+    /**
      * Put a short phrase describing the static method myMethod here.
      */
     private static void printDone() {
@@ -111,6 +122,8 @@ public final class ExcelMakerConsole {
         SimpleReader in = new SimpleReader1L();
         SimpleWriter out = new SimpleWriter1L();
 
+        SimpleWriter fileOut = new SimpleWriter1L("xmlSchedule.txt");
+
         /*
          * Prompt for and record the number of names
          */
@@ -146,6 +159,94 @@ public final class ExcelMakerConsole {
 
         }
 
+        /*
+         * Record info, iterate through all students
+         */
+        for (int i = 0; i < numOfPeople; i++) {
+            out.println();
+            out.println();
+            out.println(namesList[i]);
+            outputName(namesList[i], classNumList[i], fileOut);
+
+            /*
+             * prompt for class info, iterate for however many classes were
+             * specified.
+             */
+            for (int j = 0; j < classNumList[i]; j++) {
+                out.println();
+                out.print("Class " + (j + 1) + ": ");
+
+                //set temp String equal to the user input class name
+                String className = in.nextLine();
+
+                //output the class name as a tag to the xml schedule file
+                outputClass(className, fileOut);
+
+                out.println();
+
+                /*
+                 * declare classType boolean array. Used to determine existence
+                 * of LEC REC or LAB sessions per class
+                 */
+                boolean[] classType = new boolean[NUMOFCLASSTYPES];
+
+                for (int k = 0; k < NUMOFCLASSTYPES; k++) {
+                    //initialize the temporary variable that holds user input
+                    String yesOrNo = "placeHolder";
+
+                    //keep asking if user does not input y/n
+                    while (!yesOrNo.equals("y") && !yesOrNo.equals("n")) {
+                        //prompt for input
+                        out.print(CLASSTYPENAMES[k] + " (y/n): ");
+                        //store input
+                        yesOrNo = in.nextLine();
+                        //if input is y/n, record in boolean array classType
+                        if (yesOrNo.equals("y") || yesOrNo.equals("n")) {
+                            classType[k] = (yesOrNo.equals("y"));
+                        } else {
+                            //if input is not y/n, inform user
+                            out.println(
+                                    "Error. Please input either (y)es or (n)o");
+                        }
+
+                    }
+
+                }
+
+                /*
+                 * Arrays used to store user info on classes. These will later
+                 * be fed into the outputClassInfo function.
+                 */
+                String[] start = new String[NUMOFCLASSTYPES];
+                String[] end = new String[NUMOFCLASSTYPES];
+                String[] location = new String[NUMOFCLASSTYPES];
+
+                for (int l = 0; l < NUMOFCLASSTYPES; l++) {
+                    //if the session exists for this class, prompt and record
+                    if (classType[l]) {
+                        out.println();
+                        out.println(CLASSTYPENAMES[l]);
+                        out.print("Start time: ");
+                        start[l] = in.nextLine();
+                        out.print("End time: ");
+                        end[l] = in.nextLine();
+                        out.print("Location: ");
+                        location[l] = in.nextLine();
+                    } else {
+                        //If session does not exist for this class, input null values
+                        start[l] = "N/A";
+                        end[l] = "N/A";
+                        location[l] = "N/A";
+                    }
+
+                }
+                //print the class information in an xml file
+                outputClassInfo(classType, start, end, location, fileOut);
+            }
+            //print the footer of the student tag
+            outputStudentFooter(fileOut);
+        }
+
 //        out.println("You typed these names:");
 //        for (String name : namesList) {
 //            out.println(name);
@@ -161,6 +262,7 @@ public final class ExcelMakerConsole {
         printDone();
         in.close();
         out.close();
+        fileOut.close();
     }
 
 }
